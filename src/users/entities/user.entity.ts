dc7e49f4ca6b748c563ea0e550fde748dfc8e83e
Josh Teng <joshteng@me.com>
2020-11-15 20:52:11 +0800

    create user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,20 @@
-import { Entity } from "typeorm";
+import { MainEntity } from "src/common/entities/main.entities";
+import { BeforeInsert, Column, Entity } from "typeorm";
+import * as bcrypt from 'bcrypt'
 
 @Entity()
-export class User {
-  
+export class User extends MainEntity {
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    if (this.password) {
+      this.password = await bcrypt.hash(this.password, Number(process.env.HASH_SALT))
+    }
+  }
 }
