26206afe045a0685fec72683063ccb8807623d8a
Josh Teng <joshteng@me.com>
2020-11-17 22:38:26 +0800

    uniqueness validation on email for user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,10 @@
 import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+import { Unique } from "src/common/validator/unique.validator";
+import { User } from "../entities/user.entity";
 
 export class CreateUserDto {
   @IsEmail()
+  @Unique(User)
   readonly email: string;
 
   @IsNotEmpty()
